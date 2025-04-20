244d12ba88d5da3b57b975824f26d841851761c1
Devyn Johnson <dljohnson1313@gmail.com>
2025-04-20 17:58:26 -0500

    Adding CardTitle to card component to display text of the item name on the card.

--- virtualPEX/client/components/Card.jsx
+++ virtualPEX/client/components/Card.jsx
@@ -27,6 +27,13 @@ const CardImage = styled.img`
   border-radius: 8px;
 `;
 
+const CardTitle = styled.h3`
+  margin-top: 0.5rem;
+  font-size: 1rem;
+  font-weight: bold;
+  color: #333;
+`;
+
 const Card = ({ item, voiceId = 'henry' }) => {
   const { name, imageUrl } = item;
 
@@ -43,6 +50,7 @@ const Card = ({ item, voiceId = 'henry' }) => {
     <CardContainer>
       <CardButton onClick={handleClick} aria-label={`Speak ${name}`}>
         <CardImage src={imageUrl} alt={name} />
+        <CardTitle>{name}</CardTitle>
       </CardButton>
     </CardContainer>
   );
