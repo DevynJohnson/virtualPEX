12c03712a691da54b9e6b207deb434f34e38b292
Alexander Shegstad <aShegstad@my365.Bellevue.edu>
2025-04-21 19:15:20 -0500

    background styling

--- virtualPEX/client/App.jsx
+++ virtualPEX/client/App.jsx
@@ -2,11 +2,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavTabs from './components/Nav.jsx';
+import AlphabetBackground from './components/AlphabetBackground.jsx';
 import './App.css';
 
 function App() {
   return (
     <>
+      <AlphabetBackground />
       <NavTabs />
       <main>
         <Outlet />
